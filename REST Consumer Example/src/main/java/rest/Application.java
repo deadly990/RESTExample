230642a0c3aff8b05b2098bc344046c06dca699b
JJ <JJ@JJ-PC>
2017-06-12 11:52:52 -0400

    Consumed Method which returns all results.

--- REST Consumer Example/src/main/java/rest/Application.java
+++ REST Consumer Example/src/main/java/rest/Application.java
@@ -1,23 +1,35 @@
 package rest;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.web.client.RestTemplate;
 
 import rest.dowjones.Stock;
 
 public class Application {
+	//Change this to whatever address you have locally.
+	//garnerj990.zapto.org will not be available
+	public final static String URI = "http://garnerj990.zapto.org:8012/stocks";
+	public final static String PARAMURI = URI + "/{id}";
     public static void main(String args[]) {
         RestTemplate restTemplate = new RestTemplate();
-        Stock stock = restTemplate.getForObject("http://garnerj990.zapto.org:8012/stocks/1", Stock.class);
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("id", "1");
+        Stock stock = restTemplate.getForObject(PARAMURI, Stock.class, params);
         
         System.out.println(stock.toString());
         stock.setLow(new BigDecimal("999.999"));
-        restTemplate.put("http://garnerj990.zapto.org:8012/stocks/1", stock);
+        restTemplate.put(PARAMURI, stock, params);
         
-        stock = restTemplate.getForObject("http://garnerj990.zapto.org:8012/stocks/1", Stock.class);
+        stock = restTemplate.getForObject(PARAMURI, Stock.class, params);
         
+
         System.out.println(stock.toString());
+        String s = restTemplate.getForObject(URI + ".json", String.class);
+        
+        System.out.println(s);
         
     }
 }
